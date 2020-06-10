2723cf27a75a95f51af34c581c633183721b2b57
Alex Pereverzyev <pereverzev.alex@gmail.com>
2020-06-10 23:43:51 +0300

    Rework counter bounds

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const ElasticCounter = require('./lib/elastic_counter')
 const http = require('http')
 const port = 7777
 
+const ElasticCounter = require('./lib/elastic_counter')
 const counter = new ElasticCounter()
 
 const server = http.createServer((req, res) => {
     setTimeout(() => {
 
-        if (counter.over(req.url, 1)) {
+        if (counter.over(req.url, { upper: 1 })) {
             res.writeHead(429).end()
         } else {
             res.end()
