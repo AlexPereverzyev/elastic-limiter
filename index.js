ad4f0cf2400c498397ca9fe7ca118d9be5205cf5
Alex Pereverzyev <pereverzev.alex@gmail.com>
2020-06-15 00:24:45 +0300

    Add circuit breaker

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 module.exports = { 
+    ElasticFactory: require('./lib/elastic_factory'),
     ElasticCounter: require('./lib/elastic_counter'),
     create: function () {
-        return new this.ElasticCounter()
+        return new this.ElasticFactory()
     },
     global: function () {
         return this.instance || (this.instance = this.create())
